bd33087bacd1d96e768072064851216d707ed37c
daimessdn@gmail.com <daimessdn@gmail.com>
2023-07-16 22:00:51 +0700

    added graph demonstration for Newton-Raphson

--- newton-raphson.py
+++ newton-raphson.py
@@ -4,24 +4,26 @@
 
 import time
 
-def f(x: int | float):
+import matplotlib.pyplot as plt
+
+def f(x):
     """
     define the y as a function of x
-    let say f(x) = x^2 + 4x - 32
+    let say f(x) = x^2 - 6x - 16
     """
 
-    return x**2 + 4 * x - 32
+    return (x**2) - 6 * x - 16
 
-def diff_f(x: int | float):
+def diff_f(x):
     """
     define the differential or derivative of y
     as a function of x
-    in case of y = f(x) = x^2 + 4x - 32, dy/dx = 2x + 4
+    in case of y = f(x) = x^2 - 6x - 16, dy/dx = 2x - 6
     """
 
-    return 2 * x + 4
+    return 2 * x - 6
 
-def newton_raphson(x: int | float, fx: int | float, dfx: int | float):
+def newton_raphson(x, fx, dfx):
     """
     Newton-Raphson method
     will be get the new x according to the slope
@@ -32,7 +34,12 @@ def newton_raphson(x: int | float, fx: int | float, dfx: int | float):
 
 iter = 0
 epsilon = 0.000000001
-x0 = 10
+x0 = 25
+
+x = [i * 0.25 for i in range(-25, 100 + 1)]
+y = [f(i * 0.25) for i in range(-25, 100 + 1)]
+
+plt.plot(x, y, label="f(x) = x^2 - 6x - 16")
 
 while (True):
     x1 = newton_raphson(x0, f(x0), diff_f(x0))
@@ -41,9 +48,24 @@ while (True):
         break
     
     iter += 1
+    plt.plot([x0, x1], [f(x0), 0], label=("iter " + str(iter)))
     x0 = x1
     
     # print(x0)
     # time.sleep(1)
 
 print("x =", round(x0, 2), "with", iter, "iterations")
+print("Newton-Raphson iteration plotted")
+
+plt.title("Newton-Raphson Method\nfor x^2 - 6x - 16")
+plt.xlabel("x")
+plt.xlim(0, 25)
+plt.ylim(0, f(25))
+plt.ylabel("y = f(x)")
+
+plt.scatter(x0, f(x0), label=("x found: " + str(round(x0, 2))))
+
+plt.legend()
+
+# plt.show()
+plt.savefig("newton-raphson-result.png")
